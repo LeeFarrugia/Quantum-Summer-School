8a329ca9ee9462a93268253d89d418f194670d8e
Lee Farrugia <farrugialee@gmail.com>
2022-09-08 11:59:28 +0200

    Sending message

--- Qalypso-Hackathon.py
+++ Qalypso-Hackathon.py
@@ -1,20 +1,31 @@
 from qiskit import *
 import matplotlib.pyplot as plt
+import numpy as np
+from numpy.random import randint
+
+n = 1
+s_bits = randint(2, size=n)
+s_bases = randint(2, size=n)
 
 # creation of teleportation circuit
-c = QuantumCircuit(3, 3)
-c.x(0)
-c.barrier()
-c.h(1)
-c.cx(1,2)
-c.cx(0,1)
-c.h(0)
-c.barrier()
-c.measure([0,1], [0,1])
-c.barrier()
-c.cx(1,2)
-c.cz(0,2)
-c.draw('mpl')
-plt.show()
+def sending_message(bits, bases):
+    message = []
+    for i in range(n):
+        c = QuantumCircuit(3, 3)
+        if bases[i] == 0:
+            if bits[i] == 0:
+                pass
+            else:
+                c.x(0)
+        else:
+            if bits[i] == 0:
+                    c.h(1)
+            else:
+                c.cx(1,2)
+                c.cx(0,1)
+        c.barrier()
+        message.append(c)
+    return message
 
+print(sending_message(s_bits, s_bases))
 
